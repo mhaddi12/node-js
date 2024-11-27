55f276f07e67fcf9849d8a06d00a22902adcf4c8
Muhammad Haddi <se.haddi1@gmail.com>
2024-11-27 11:15:43 -0800

    add auth

--- task management api/routers/task.router.js
+++ task management api/routers/task.router.js
@@ -7,8 +7,13 @@ const {
   deleteTask,
   updateTask,
   completeTask,
-  getTasksByCompletion
+  getTasksByCompletion,
 } = require("../controllers/task.controller");
+
+
+const { register, login } = require("../controllers/user.controller");
+
+
 
 router.post("/createTask", createTask);
 router.get("/getTask", getTasks);
@@ -17,5 +22,10 @@ router.delete("/deleteTask/:id", deleteTask);
 router.put("/updateTask/:id", updateTask);
 router.put("/completeTask/:id", completeTask);
 router.get("/getTasksByCompletion", getTasksByCompletion);
+
+//regsiter
+
+router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;
